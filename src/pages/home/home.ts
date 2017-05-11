a65d8ff776e63b1cdc7cea9764efd12fe1459aa0
Victor Eliasson <victor.eliasson@webdeziner.se>
2017-05-11 19:42:28 +0200

    Page updates
    Get closest stations by geolocation
    Ability to save API key to local storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, LoadingController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { ApiProvider } from '../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -7,9 +9,32 @@ import { NavController, IonicPage } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public latitude:number;
+  public longitude:number;
+  public nearbyLocations: any;
+  public loader: any;
+  constructor(public navCtrl: NavController, public geo: Geolocation, public api: ApiProvider, public loadingCtrl: LoadingController) {
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait...",
+    });
+    this.loader.present();
+  }
 
-  constructor(public navCtrl: NavController) {
+  ionViewDidEnter() {
+    this.geo.getCurrentPosition().then((response) => {
+      this.latitude = response.coords.latitude;
+      this.longitude = response.coords.longitude;
+      this.api.getNearbyStops(this.latitude, this.longitude).then((response) => {
+        this.loader.dismiss();
+        this.nearbyLocations = response;
+      }).catch((error) => {
+        console.log("home.ts 31: ",error);
+      });
+    });
+  }
 
+  showDetails(item) {
+    this.navCtrl.push('DetailsPage', {item: item});
   }
 
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { ApiProvider } from '../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -7,12 +10,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public settingsForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, formBuilder: FormBuilder, public storage: Storage, public api: ApiProvider) {
+    this.api.getAllKeys().then(() => {
+        this.settingsForm = formBuilder.group({
+          nearbyKey: [this.api.nearby_api],
+          locationsKey: [this.api.locations_api],
+          realtimeKey: [this.api.realtime_api]
+        });
+      });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  saveSettings() {
+    let nearby_key = this.settingsForm.controls['nearbyKey'].value;
+    let locations_key = this.settingsForm.controls['locationsKey'].value;
+    let realtime_key = this.settingsForm.controls['realtimeKey'].value;
+
+    this.storage.set('nearby_key', nearby_key);
+    this.storage.set('locations_key', locations_key);
+    this.storage.set('realtime_key', realtime_key);
   }
 
 }
